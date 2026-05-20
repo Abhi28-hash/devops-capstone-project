7c6df7c1916b554451a1066922838db45b9a3a97
Abhi28-hash <bnabhi028@gmail.com>
2026-05-20 11:06:22 -0400

    Implemented read account functionality

--- service/models.py
+++ service/models.py
@@ -1,9 +1,7 @@
-from itertools import count
-
 class Account:
 
-    ids = count(0)
-    data = {}
+    _accounts = []
+    _counter = 0
 
     def __init__(self, name, email):
         self.id = None
@@ -11,20 +9,30 @@ class Account:
         self.email = email
 
     def create(self):
-        self.id = next(Account.ids)
-        Account.data[self.id] = self
+        Account._counter += 1
+        self.id = Account._counter
+        Account._accounts.append(self)
         return self
 
     def update(self):
-        Account.data[self.id] = self
+        for idx, account in enumerate(Account._accounts):
+            if account.id == self.id:
+                Account._accounts[idx] = self
+                return self
 
     def delete(self):
-        del Account.data[self.id]
+        Account._accounts = [
+            account for account in Account._accounts
+            if account.id != self.id
+        ]
 
     @classmethod
     def find(cls, account_id):
-        return cls.data.get(account_id)
+        for account in cls._accounts:
+            if account.id == account_id:
+                return account
+        return None
 
     @classmethod
     def list_all(cls):
-        return list(cls.data.values())
+        return cls._accounts
